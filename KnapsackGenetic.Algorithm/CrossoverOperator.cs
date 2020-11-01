d1966de8edf5c78f050848ea801867ea180911bf
Marius Vladu <marius.vladu96@gmail.com>
2020-11-01 12:19:12 +0200

    Added CrossoverOperator

---
+++ KnapsackGenetic.Algorithm/CrossoverOperator.cs
@@ -0,0 +1,34 @@
+﻿using KnapsackGenetic.Algorithm.Contracts;
+using KnapsackGenetic.Domain;
+using System;
+
+namespace KnapsackGenetic.Algorithm
+{
+    public class CrossoverOperator : ICrossoverOperator
+    {
+        public Tuple<Individual, Individual> GetOffsprings(Individual parent1, Individual parent2, double crossoverRate)
+        {
+            var numberOfGenes = parent1.Genes.Length;
+            var splitIndex = (int)(numberOfGenes * crossoverRate);
+
+            var offspring1 = new Individual { Genes = new byte[numberOfGenes] };
+            var offspring2 = new Individual { Genes = new byte[numberOfGenes] };
+
+            for (int i = 0; i < numberOfGenes; i++)
+            {
+                if(i <= splitIndex)
+                {
+                    offspring1.Genes[i] = parent1.Genes[i];
+                    offspring2.Genes[i] = parent2.Genes[i];
+                }
+                else
+                {
+                    offspring1.Genes[i] = parent2.Genes[i];
+                    offspring2.Genes[i] = parent1.Genes[i];
+                }
+            }
+
+            return new Tuple<Individual, Individual>(offspring1, offspring2);
+        }
+    }
+}
--- KnapsackGenetic.Algorithm/GeneticAlgorithm.cs
+++ KnapsackGenetic.Algorithm/GeneticAlgorithm.cs
@@ -11,30 +11,59 @@ namespace KnapsackGenetic.Algorithm
         private readonly IFitnessFunction fitnessFunction;
         private readonly ISelectionOperator selectionOperator;
         private readonly ISelectionOperator elitistSelection;
+        private readonly ICrossoverOperator crossoverOperator;
 
         private readonly List<Item> items;
         private readonly int weightLimit;
         private readonly int numberOfGenes;
+        private readonly double crossoverRate;
 
         private List<Individual> currentPopulation;
         private List<Solution> currentSolutions;
 
-        public GeneticAlgorithm(List<Item> items, int weightLimit, int initialPopulationSize,
+        public GeneticAlgorithm(List<Item> items, int weightLimit, double crossoverRate, int initialPopulationSize,
             IInitialPopulationProvider initialPopulationProvider,
-            IFitnessFunction fitnessFunction, 
-            ISelectionOperator selectionOperator, 
-            ISelectionOperator elitistSelection)
+            IFitnessFunction fitnessFunction,
+            ISelectionOperator selectionOperator,
+            ISelectionOperator elitistSelection, 
+            ICrossoverOperator crossoverOperator)
         {
             this.items = items;
             this.weightLimit = weightLimit;
+            this.crossoverRate = crossoverRate;
             this.initialPopulationProvider = initialPopulationProvider;
             this.fitnessFunction = fitnessFunction;
             this.selectionOperator = selectionOperator;
             this.elitistSelection = elitistSelection;
+            this.crossoverOperator = crossoverOperator;
 
             numberOfGenes = items.Count;
             currentPopulation = initialPopulationProvider.GetInitialPopulation(initialPopulationSize, numberOfGenes);
             currentSolutions = GetCurrentSolutions();
+        }
+
+        public List<Solution> ComputeNextGeneration()
+        {
+            var numberOfCrossovers = currentSolutions.Count / 2 - 2;
+
+            for (int i = 0; i < numberOfCrossovers; i++)
+            {
+                var parent1 = selectionOperator.SelectOne(currentSolutions);
+                RemoveFromCurrentSolutions(parent1);
+
+                var parent2 = selectionOperator.SelectOne(currentSolutions);
+                RemoveFromCurrentSolutions(parent2);
+
+                var offsprings = crossoverOperator.GetOffsprings(parent1, parent2, crossoverRate);
+
+            }
+        }
+
+        private void RemoveFromCurrentSolutions(Individual individual)
+        {
+            var indexToRemove = currentSolutions.FindIndex(s => s.Individual == individual);
+
+            currentSolutions.RemoveAt(indexToRemove);
         }
 
         private List<Solution> GetCurrentSolutions()
